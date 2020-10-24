000f716f77f77900be8214d721bbcb82bbb47d3d
red-tt-bear <red.tt.bear@gmail.com>
2020-10-24 03:35:49 -0400

    Line number trial

--- src/Utilities/Logger.ts
+++ src/Utilities/Logger.ts
@@ -2,6 +2,8 @@ import winston from 'winston';
 import AxiosRequest from '../Hooks/AxiosRequest';
 import AxiosBatchTransport from './AxiosBatchTransport';
 import BrowserConsoleLoggerTransport from './BrowserConsoleLoggerTransport';
+import * as sourcemappedStacktrace from 'sourcemapped-stacktrace';
+import urljoin from 'url-join';
 
 let level = process.env.NODE_ENV === 'production' ? 'error' : 'debug';
 
@@ -17,15 +19,14 @@ const transports = {
         axios: AxiosRequest,
         loggingEndpoint: '/utility/client-logs',
         format: winston.format.simple(),
-    })
+    }),
 };
 
 const logger = winston.createLogger({
     level: level,
     format: winston.format.simple(),
-    defaultMeta: { service: 'user-service' },
+    defaultMeta: {},
     transports: Object.values(transports),
-    
 });
 
 declare global {
@@ -41,6 +42,21 @@ window.setLogLevel = (level: string) => {
     transports.console.level = level;
 };
 
-
-// const logger = console;
+Object.keys(logger.levels).forEach((level: string) => {
+    const levelKey = level as keyof winston.Logger;
+    const origLog = logger[levelKey] as any;
+    (logger as any)[levelKey] = (...args: any[]) => {
+        sourcemappedStacktrace.mapStackTrace(new Error('For get stack purposes only').stack, (result: Array<string>) => {
+            const parsed = /\((.*):(\d+):(\d+)\)/.exec(result[1]);
+            if (parsed !== null) {
+                const [full, filePath, lineNumber, columnNumber] = parsed;
+                const url = urljoin(window.origin, full.substring(1, full.length - 1));
+                args.push({filePath, lineNumber, columnNumber}, url);
+            } else {
+                args.push(result[1]);
+            }
+            origLog(...args);
+        });
+    };
+});
 export default logger;
